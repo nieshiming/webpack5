7e134ae59d57424531a329d94687793e61c2a30d
nieshiming <shiming.nie@aihuishou.com>
2021-08-19 18:59:54 +0800

    chore: 1111

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,8 @@ import React, { Suspense } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 
+console.log(111)
+
 const App = () => (
   <BrowserRouter>
     <Suspense fallback={<span>loading...</span>}>{renderRoutes(routes)}</Suspense>
