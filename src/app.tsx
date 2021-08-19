7a4d47fc7a2181bc9966f16ac50333cac01c86ac
nieshiming <shiming.nie@aihuishou.com>
2021-08-19 19:52:25 +0800

    ui: adjust

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,6 @@ import React, { Suspense } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 
-console.log(111)
-
 const App = () => (
   <BrowserRouter>
     <Suspense fallback={<span>loading...</span>}>{renderRoutes(routes)}</Suspense>
